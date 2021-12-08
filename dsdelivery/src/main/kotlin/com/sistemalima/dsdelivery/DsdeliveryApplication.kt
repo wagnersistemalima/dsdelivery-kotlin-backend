420ed0162a047789979b48684cdd6a42467d59ac
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-12-08 16:35:07 -0300

    projeto inicial criado

---
+++ dsdelivery/src/main/kotlin/com/sistemalima/dsdelivery/DsdeliveryApplication.kt
@@ -0,0 +1,11 @@
+package com.sistemalima.dsdelivery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DsdeliveryApplication
+
+fun main(args: Array<String>) {
+	runApplication<DsdeliveryApplication>(*args)
+}
